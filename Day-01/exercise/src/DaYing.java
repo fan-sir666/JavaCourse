5c355011ef99fbf7650c3dda2283a7d60b8447b2
fan-sir666 <fan_sir666@163.com>
2021-03-15 22:59:20 +0800

    添加:今日课堂练习(2021.03.15)

---
+++ Day-01/exercise/src/DaYing.java
@@ -0,0 +1,15 @@
+public class DaYing {
+    public static void main(String[] args) {
+//        第一行
+        System.out.print("***");
+        System.out.println(" ");
+
+//        第二行
+        System.out.print("***");
+        System.out.println(" ");
+
+//        第三行
+        System.out.print("***");
+        System.out.println(" ");
+    }
+}
---
+++ Day-01/exercise/src/HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("来了老弟,HelloWorld!!!");
+    }
+}
