26873c3f4baee6858502929ef073328ab233d355
fan-sir666 <fan_sir666@163.com>
2021-03-25 18:27:38 +0800

    添加:今日代码练习

--- Day-05/exercise/src/SanYuan.java
+++ Day-05/exercise/src/SanYuan.java
@@ -5,5 +5,31 @@ public class SanYuan {
         *
         *   注意: 三元运算的结果必须被使用（赋值   运算   展示）
         * */
+
+//        int a = 10;
+//        int b = 20;
+//        int c = a>b ? 100 : 200;
+//        System.out.println(c); // 200
+
+
+//        int a = 10;
+//        int b = 20;
+//        a+=20; // 30
+//        b+=10; // 30
+//        String s = a>=b ? "蛋糕":"月饼";
+//        System.out.println(s); // 蛋糕
+
+//        int a = 10;
+//        int b = 20;
+//        int c = 30;
+//        //定义一个临时的变量 用来保存两个数中的较大值
+//        int tmp = a > b ? a : b;
+//        //利用上一步的较大值跟第三个数比较，确定这三个数中的最大值
+//        int max = c > tmp ? c : tmp;
+//        //打印结果
+//        int max = c > (a>b?a:b) ? c : (a>b?a:b);
+//        System.out.println(max); // 30
+
+
     }
 }
---
+++ Day-06/exercise/src/PanDuan/Demo01.java
@@ -0,0 +1,55 @@
+package PanDuan;
+
+public class Demo01 {
+    public static void main(String[] args) {
+        /*
+        *  流程语句：if
+        *
+        *    格式:
+        *      单条件： if(判断条件){代码块}
+        *      二选一:  if(判断条件){代码块} else {代码块}
+        *      多条件:  if(判断条件){代码块} else if(判断条件){代码块} else {代码块}
+        * */
+
+//        int age = 20;
+//        if (age >= 18) {
+//            System.out.println("成年人：今年"+age+"岁");
+//        }
+
+
+//        String name = "范志伟";
+//        int sex = 1;
+//        if (sex == 0) {
+//            System.out.println("欢迎"+name+"女士");
+//        }else {
+//            System.out.println("欢迎"+name+"先生");
+//        }
+
+//        int num = 10;
+//        switch (num){
+//            case 1:
+//            case 2:
+//            case 3:
+//            case 4:
+//            case 5:
+//                System.out.println("今天是工作日");
+//                break;
+//            case 6:
+//            case 7:
+//                System.out.println("今天是休息日");
+//                break;
+//                default:
+//                    System.out.println("输入数字超出范围1~7");
+//        }
+
+//        int score = 59;
+//        if (score >= 80) {
+//            System.out.println("成绩优秀");
+//        }else if (score >= 60 && score < 79) {
+//            System.out.println("成绩及格");
+//        }else {
+//            System.out.println("需要再次补考");
+//        }
+
+    }
+}
