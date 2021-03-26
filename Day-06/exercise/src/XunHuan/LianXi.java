a46b0411733b41e5bfe361c2dc220f4280485de7
fan-sir666 <fan_sir666@163.com>
2021-03-26 19:58:50 +0800

    添加:今日代码练习

---
+++ Day-06/exercise/src/XunHuan/LianXi.java
@@ -0,0 +1,31 @@
+package XunHuan;
+
+public class LianXi {
+    public static void main(String[] args) {
+        //思考案例1：要求，每次展示一个*，用for展示如下效果 （不能用if）
+//	*
+//	**
+//	***
+//	****
+
+//        for (int i = 0; i < 4; i++) {
+//            for (int j = 0; j <= i ; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println(" ");
+//        }
+
+//      思考案例2：要求，每次展示一个*，用for展示如下效果 （不能用if）
+//	****
+//	***
+//	**
+//	*
+
+//        for (int i = 0; i < 4; i++) {
+//            for (int j = 4; j > i ; j--) {
+//                System.out.print("*");
+//            }
+//            System.out.println(" ");
+//        }
+    }
+}
